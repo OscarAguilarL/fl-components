a14d875a890f0230f41661d099e7b3dd05092276
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-01-05 20:26:15 -0600

    Crear ListTiles a partir de una lista estatica

---
+++ lib/screens/listview1_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ListView1Screen extends StatelessWidget {
+  final options = const [
+    'Megaman',
+    'MetalGear',
+    'Super Smash',
+    'Final Fantasy'
+  ];
+
+  const ListView1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListView tipoi 1'),
+      ),
+      body: ListView(
+        children: [
+          ...options
+              .map(
+                (game) => ListTile(
+                  title: Text(game),
+                  trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                ),
+              )
+              .toList(),
+        ],
+      ),
+    );
+  }
+}
