28b8cf373cf9cadee98d969cd8f54c9cfc3f900e
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-01-09 19:40:57 -0600

    Mostrar una alerta

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,6 +3,36 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialog(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusDirectional.circular(10),
+          ),
+          title: const Text('Hola mundo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('Este es el contenido de la alerta'),
+              SizedBox(height: 20),
+              FlutterLogo(size: 100),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancelar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,13 +42,11 @@ class AlertScreen extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
             child: Text('Mostrar Alerta', style: TextStyle(fontSize: 20)),
           ),
-          onPressed: () {},
+          onPressed: () => displayDialog(context),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
+        onPressed: () => Navigator.pop(context),
         child: const Icon(Icons.close),
       ),
     );
