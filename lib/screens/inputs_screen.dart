de64d47c9d5441f4ba7cd743ad3bf0912f468f10
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-01-14 17:21:05 -0600

    Centralizar el estilo de los inputs

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class InputsScreen extends StatelessWidget {
@@ -13,37 +14,10 @@ class InputsScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
-            children: [
-              TextFormField(
-                autofocus: true,
-                initialValue: '',
-                textCapitalization: TextCapitalization.words,
-                onChanged: (value) {},
-                validator: (value) {
-                  if (value == null) return 'Este campo es requerido';
-                  return value.length < 3 ? 'Mínimo de 3 letras' : null;
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: const InputDecoration(
-                  hintText: 'Nombre del usuario',
-                  labelText: 'Nombre:',
-                  helperText: 'Solo letras',
-                  counterText: '3 caracteres',
-                  // prefixIcon: Icon(Icons.verified_user_outlined),
-                  suffixIcon: Icon(Icons.group_outlined),
-                  icon: Icon(Icons.assignment_ind_outlined),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.green,
-                    ),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                    ),
-                  ),
-                ),
+            children: const [
+              CustomInputField(
+                labelText: 'Nombre',
+                hintText: 'Nombre de usuario',
               ),
             ],
           ),
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -30,6 +30,24 @@ class AppTheme {
         elevation: 0,
       ),
     ),
+
+    // inputDecorationTheme
+    inputDecorationTheme: const InputDecorationTheme(
+      floatingLabelStyle: TextStyle(color: primary),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: primary),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          bottomRight: Radius.circular(10),
+        ),
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          bottomRight: Radius.circular(10),
+        ),
+      ),
+    ),
   );
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
